bc8823a2722814138c8c87b4c77bfb878fd9a854
pilhwanKim <pilhwankim03@gmail.com>
2020-01-15 23:33:00 +0900

    google console

--- src/html.js
+++ src/html.js
@@ -12,6 +12,7 @@ export default class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1, maximum-scale=2"
           />
+          <meta name="google-site-verification" content="MIsQQYzIEG6CxoBmzZnd3cQbxbVjZ6GfgQaa8xPS0wY" />
           {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes}>
